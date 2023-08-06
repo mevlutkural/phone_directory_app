6df5a0d57dd3ac4c56cb5db37e5deb1aa5ac7271
sam <sam@sams-Air.mshome.net>
2023-08-06 09:17:34 +0300

    frontend and backend projects included.

---
+++ backend/app/Models/Group.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    /**
+     * The primary key (ID).
+     *
+     * @var string $primaryKey
+     */
+    protected $primaryKey = 'group_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'group_name',
+    ];
+}
---
+++ frontend/phone_directory_app/src/views/LoginPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex align-items-center" style="height: 100vh;">
+    <div class="card col-sm-4 mx-auto px-3 py-2">
+      <div class="row">
+        <div class="mb-4 text-center display-5 fs-4 fw-bold">Phone Directory App | Login</div>
+        <div class="mb-1">
+          <label for="email"></label>
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            placeholder="E-Mail"
+          />
+        </div>
+        <div class="mb-1">
+          <label for="password"></label>
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <div class="mb-1">
+          <label for="password"></label>
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
